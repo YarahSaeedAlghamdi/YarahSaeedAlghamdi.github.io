9902c32800fddedad7738e16f993d47ecdab20f6
Yarah Alghamdi <157582634+YarahSaeedAlghamdi@users.noreply.github.com>
2024-11-07 20:53:42 +0300

    newer update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "410895373"
+    serviceWorkerVersion: "4226087315"
   }
 });
